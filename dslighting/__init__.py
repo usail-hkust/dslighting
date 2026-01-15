866c65f27cdc94a4e80c1abee3174b1e0fda273b
anonymous-review-lab <94355726+luckyfan-cs@users.noreply.github.com>
2026-01-15 19:17:21 +0800

    feat: release v1.0.4 with aiohttp compatibility fix for Python 3.9
    - Add aiohttp<3.10 constraint for Python 3.9 compatibility
    - Fixes TypeError: unhashable type: 'list' issue
    - Ensures smooth installation on Python 3.9 systems
    - PyPI: https://pypi.org/project/dslighting/1.0.4/

--- dslighting/__init__.py
+++ dslighting/__init__.py
@@ -26,7 +26,7 @@ Advanced Usage:
 For more information, see: https://github.com/usail-hkust/dslighting
 """
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __author__ = "DSLighting Team"
 
 # Core API classes
