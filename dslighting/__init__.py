d17b0dbd273f6db2940fd386863449f6ef54dce8
anonymous-review-lab <94355726+luckyfan-cs@users.noreply.github.com>
2026-01-21 22:53:23 +0800

    chore: bump version to 2.4.1

--- dslighting/__init__.py
+++ dslighting/__init__.py
@@ -17,7 +17,7 @@ Quick Start:
 Documentation: https://github.com/usail-hkust/dslighting
 """
 
-__version__ = "2.4.0"
+__version__ = "2.4.1"
 __author__ = "DSLighting Team"
 
 # ============================================================================
